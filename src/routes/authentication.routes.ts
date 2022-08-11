818d24af6e8b94c951eca3cc73dbb16d554b8154
mateuscavedini <mateuscvdn@gmail.com>
2022-08-11 14:15:21 -0300

    added authentication routes

---
+++ src/routes/authentication.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { AuthenticationController } from "../controllers/authentication.controller";
+import { basicAuthenticationMiddleware } from "../middlewares/basicAuthentication.middleware";
+import { jwtAuthenticationMiddleware } from "../middlewares/jwtAuthentication.middleware";
+
+export const authenticationRoutes = Router()
+
+const authenticationController = new AuthenticationController()
+
+// get JWT token
+authenticationRoutes.post("/token", basicAuthenticationMiddleware, authenticationController.getJwtToken)
+
+// validate JWT token
+authenticationRoutes.post("/token/validade", jwtAuthenticationMiddleware, authenticationController.validateJwtToken)
